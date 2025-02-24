c1d32aa6622071d3b56a577e63d9a1c7eba109e2
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-02-24 14:54:27 -0500

    Organized the project so that there are groups for views, the model, and view models.

---
+++ MVVMAndFreeformInput/Views/PowerView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Nicholas Hwang on 24/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
