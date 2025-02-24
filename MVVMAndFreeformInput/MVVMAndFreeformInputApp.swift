97299ea61b4e09c03b5d66b6b9b1f4106f9f7e3e
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-02-24 14:50:53 -0500

    Initial Commit

---
+++ MVVMAndFreeformInput/MVVMAndFreeformInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeformInputApp.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Nicholas Hwang on 24/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAndFreeformInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
